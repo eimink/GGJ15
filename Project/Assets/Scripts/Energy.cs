346788ede4afe7ce0c3e561228862efb2fb3e1fe
Mikko Romppainen <mikko.romppainen@kajak.fi>
2015-01-24 14:53:23 +0200

    Added bomb functionality

---
+++ Project/Assets/Scripts/Energy.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Energy : MonoBehaviour {
+
+	public float maxEnergy = 10.0f;
+	private float curEnergy;
+
+	// Use this for initialization
+	void Start () {
+		curEnergy = maxEnergy;
+	}
+
+	void ApplyDamage( float dmg )
+	{
+		Debug.Log("Gameobject: " + gameObject.name + " takes damage " + dmg.ToString());
+
+		curEnergy -= dmg;
+		if (curEnergy <= 0.0f)
+		{
+			Debug.Log("Gameobject: " + gameObject.name + " killed");
+			Invoke ("Destroy", 0.1f);
+		}
+	}
+
+	void Destroy()
+	{
+		Destroy (this.gameObject);
+	}
+
+	void Heal( float h )
+	{
+		Debug.Log("Gameobject: " + gameObject.name + " heals " + h.ToString());
+		curEnergy = Mathf.Clamp (curEnergy + h, 0, maxEnergy);
+	}
+
+}
--- Project/Assets/Scripts/TimeBomb.cs
+++ Project/Assets/Scripts/TimeBomb.cs
@@ -4,27 +4,60 @@ using System.Collections;
 public class TimeBomb : MonoBehaviour {
 
 	public float fuseDelay = 2.0f;
-	public float explosionRadius = 2.0f;
+	public float explosionRadius = 4.0f;
 	public float maxDamage = 10.0f;
-	public float explosionForce = 10.0f;
-	public float distanceFalloff = 2.0f;
+	public float explosionForce = 1000.0f;
+	public float distanceFalloff = 0.1f;
 
 	// Use this for initialization
 	void Start () {
 		Invoke ("Explode", fuseDelay);
 	}
 
+	void ApplyDamage(string tag)
+	{
+		GameObject [] objects = GameObject.FindGameObjectsWithTag(tag);
+		for (int i=0; i<objects.Length; ++i)
+		{
+			Vector3 delta = objects[i].GetComponent<Transform>().position - gameObject.GetComponent<Transform>().position;
+			if( delta.magnitude <= explosionRadius )
+			{
+				float dmg = (explosionRadius - delta.magnitude) / explosionRadius;
+				objects[i].SendMessage("ApplyDamage",dmg*maxDamage,SendMessageOptions.DontRequireReceiver);
+
+				CharacterController controller = objects[i].GetComponent<CharacterController> ();
+				if (controller != null)
+				{
+				//	Debug.Log("fdfsdf");
+				//	controller.Move(5.0f*dmg * delta.normalized);
+				}
+			}
+		}
+	}
+
 	void Explode()
 	{
 		this.gameObject.GetComponent<ParticleSystem>().Play();
-		RaycastHit[] hits = Physics.SphereCastAll (transform.position, explosionRadius, transform.forward);
+
+		ApplyDamage ("Player1");
+		ApplyDamage ("Player2");
+		/*RaycastHit[] hits = Physics.SphereCastAll(transform.position, explosionRadius, Vector3.forward, Mathf.Infinity);
 		foreach (RaycastHit hit in hits)
 		{
-			float dmg = maxDamage - hit.distance * distanceFalloff;
-			hit.collider.gameObject.SendMessage("ApplyDamage",dmg,SendMessageOptions.DontRequireReceiver);
-			if (hit.rigidbody != null)
-				hit.rigidbody.AddExplosionForce(explosionForce,transform.position,explosionRadius);
-		}
+			if( hit.collider.gameObject.tag == "Player1" || hit.collider.gameObject.tag == "Player2" )
+			{
+				Debug.Log( "Name: " + hit.collider.gameObject.name +  " hit.distance: " + hit.distance.ToString() );
+				if( hit.distance <= explosionRadius )
+				{
+				//	float dmg = Mathf.Clamp(maxDamage - hit.distance * distanceFalloff, 0.0f, maxDamage);
+					hit.collider.gameObject.SendMessage("ApplyDamage",4.5f,SendMessageOptions.DontRequireReceiver);
+					if (hit.rigidbody != null)
+						hit.rigidbody.AddExplosionForce(explosionForce,transform.position,explosionRadius);
+				}
+			}
+		}*/
+
+
 		Invoke ("Destroy", 2.0f);
 	}
 	void Destroy()
