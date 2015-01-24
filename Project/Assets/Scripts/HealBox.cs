e02e846539288bcf89e4427ee7b265d190a22862
Mikko Romppainen <mikko.romppainen@kajak.fi>
2015-01-24 16:21:33 +0200

    heal less

--- Project/Assets/Scripts/HealBox.cs
+++ Project/Assets/Scripts/HealBox.cs
@@ -5,9 +5,8 @@ public class HealBox : MonoBehaviour {
 
 	public float fuseDelay = 2.0f;
 	public float activityDuration = 2.0f;
-//	public float timeBetweenTicks = 0.1f;
 	public float healRadius = 2.0f;
-	public float healAmountPerSecond = 10.0f;
+	public float healAmountPerSecond = 5.0f;
 	public float distanceFalloff = 2.0f;
 
 	void ApplyHeal(string tag)
@@ -25,8 +24,8 @@ public class HealBox : MonoBehaviour {
 	}
 
 	// Use this for initialization
-	void Start () {
-	//	InvokeRepeating("Heal", fuseDelay, timeBetweenTicks);
+	void Start ()
+	{
 		Invoke("Destroy", activityDuration);
 	}
 	
