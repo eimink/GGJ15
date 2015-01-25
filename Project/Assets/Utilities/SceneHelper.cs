ddbb0f30bc1cd5a92e5c5d30750be3e47142c2e5
Teppo Kauppinen <eimink@gmail.com>
2015-01-25 13:43:05 +0200

    Scenehelper

---
+++ Project/Assets/Utilities/SceneHelper.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneHelper : MonoBehaviour {
+
+	public static SceneHelper instance;
+
+	public string levelSeed = "kekkonen";
+	public int numOfCreatures = 4;
+
+	void Awake ()
+	{
+		DontDestroyOnLoad (this.transform.gameObject);
+		if (instance == null)
+			instance = this;
+		else
+			Destroy(this.gameObject);
+	}
+}
+
+
