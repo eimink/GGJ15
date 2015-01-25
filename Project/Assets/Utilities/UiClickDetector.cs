1f60142169564104795dd5e3a16bb02ca5d2f5ef
Teppo Kauppinen <eimink@gmail.com>
2015-01-25 03:46:48 +0200

    Ui click detector helper

---
+++ Project/Assets/Utilities/UiClickDetector.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UiClickDetector : MonoBehaviour {
+
+	GameObject targetObject;
+	string targetMethod;
+
+	void OnMouseUpAsButton()
+	{
+		targetObject.SendMessage (targetMethod);
+	}
+}
